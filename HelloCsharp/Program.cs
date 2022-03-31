01229b9b0aa7ac406c3edc318da0287ef60b19a7
LucaNotaro95 <100934253+LucaNotaro95@users.noreply.github.com>
2022-03-31 17:10:52 +0200

    hello-csharp

---
+++ HelloCsharp/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ciao e benvenuto");
+Console.WriteLine("Gennaio");
+Console.WriteLine("Febbraio");
+Console.WriteLine("Marzo");
+Console.WriteLine("Aprile");
+Console.WriteLine("Maggio");
+Console.WriteLine("Giugno");
+Console.WriteLine("Luglio");
+Console.WriteLine("Agosto");
+Console.WriteLine("Settembre");
+Console.WriteLine("Ottobre");
+Console.WriteLine("novembre");
+Console.WriteLine("dicembre");
+
+Console.WriteLine("scrivi il tuo mese preferito");
+
+string mese = Console.ReadLine();
+
+Console.WriteLine("Buona giornata, a presto");
+Console.WriteLine("il tuo mese preferito è"+input);
+
+
